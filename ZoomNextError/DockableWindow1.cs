83db6bd493d2156b3c76f4d05f2014d854ef48c5
mapitech3 <69255914+mapitech3@users.noreply.github.com>
2020-08-26 09:10:44 +0300

    Update DockableWindow1.cs

--- ZoomNextError/DockableWindow1.cs
+++ ZoomNextError/DockableWindow1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -117,21 +117,48 @@ namespace ZoomNextError
                         {
                             counter++;
                             current_OID = row.OID;
-                            MessageBox.Show(current_OID.ToString());
+                            //MessageBox.Show(current_OID.ToString());
                             string GushNum = row.Value[row.Fields.FindField("GushNum")];
                             string ParcelNum = row.Value[row.Fields.FindField("ParcelNum")];
                             string GushSfx = row.Value[row.Fields.FindField("GushSfx")];
+                            object ErrDescription = row.Value[row.Fields.FindField("ErrDescription")];
 
-                            string query = "GUSH_NUM = " + GushNum +
-                            " AND PARCEL = " + ParcelNum +
-                            " AND GUSH_SUFFIX = " + GushSfx;
+                            string query = "OBJECTID < 0";
+                            if (GushNum == "0" && ParcelNum == "0" && ErrDescription != null)
+                            {
+                                if (ErrDescription.ToString().Contains("OID:"))
+                                {
+                                    //MessageBox.Show(System.Text.RegularExpressions.Regex.Match(ErrDescription.ToString(), @"\d+").Value);
+                                    string oid = System.Text.RegularExpressions.Regex.Match(ErrDescription.ToString(), @"\d+").Value;
+                                    query = "OBJECTID = " + oid;
+                                }
+                            }
+                            else
+                            {
+                                query = "GUSH_NUM = " + GushNum +
+                                    " AND PARCEL = " + ParcelNum +
+                                    " AND GUSH_SUFFIX = " + GushSfx;
+                            }
+
 
                             IFeatureLayer flayer = AO.getLayerFromMxd(map, "חלקות לעריכה");
 
                             AO.ZoomToSelection(mxd, flayer, query);
                             List_acc.Add(row.OID);
-                            MessageBox.Show(get(List_acc));
+                            //MessageBox.Show(get(List_acc));
                             row = cursor.NextRow();
+
+                            if (ErrDescription != null)
+                            {
+                                label3.Text = ErrDescription.ToString();
+                                label5.Text = sum_items.ToString() +" / " + List_acc.Count.ToString();
+                            }
+                            else
+                            {
+                                label3.Text = "ללא תיאור";
+                                label5.Text = "";
+                            }
+
                         }
                         else
                         {
@@ -166,6 +193,7 @@ namespace ZoomNextError
                 counter_items = counter_items - 1;
                 Last_OID = List_acc[List_acc.Count - 1];
                 ITable table   = AO.open_table(gdb, "Errors");
+                int sum_items = table.RowCount(null);
                 ICursor cursor = AO.create_table_cursor_with_query(table, "\"OBJECTID\" = " + Last_OID.ToString());
                 IRow row       = cursor.NextRow();
                 while (row != null)
@@ -174,21 +202,136 @@ namespace ZoomNextError
                     string GushNum = row.Value[row.Fields.FindField("GushNum")];
                     string ParcelNum = row.Value[row.Fields.FindField("ParcelNum")];
                     string GushSfx = row.Value[row.Fields.FindField("GushSfx")];
+                    object ErrDescription = row.Value[row.Fields.FindField("ErrDescription")];
 
-                    string query = "GUSH_NUM = " + GushNum +
-                    " AND PARCEL = " + ParcelNum +
-                    " AND GUSH_SUFFIX = " + GushSfx;
+                    string query = "OBJECTID < 0";
+                    if (GushNum == "0" && ParcelNum == "0" && ErrDescription != null)
+                    {
+                        if (ErrDescription.ToString().Contains("OID:"))
+                        {
+                            //MessageBox.Show(System.Text.RegularExpressions.Regex.Match(ErrDescription.ToString(), @"\d+").Value);
+                            string oid = System.Text.RegularExpressions.Regex.Match(ErrDescription.ToString(), @"\d+").Value;
+                            query = "OBJECTID = " + oid;
+                        }
+                    }
+                    else
+                    {
+                        query = "GUSH_NUM = " + GushNum +
+                            " AND PARCEL = " + ParcelNum +
+                            " AND GUSH_SUFFIX = " + GushSfx;
+                    }
 
                     IFeatureLayer flayer = AO.getLayerFromMxd(map, "חלקות לעריכה");
                     AO.ZoomToSelection(mxd, flayer, query);
                     List_acc.RemoveAt(List_acc.Count - 1);
-                    MessageBox.Show(get(List_acc));
+                    //MessageBox.Show(get(List_acc));
                     current_OID = Last_OID;
                     row = cursor.NextRow();
+
+
+                    if (ErrDescription != null)
+                    {
+                        label3.Text = ErrDescription.ToString();
+                        label5.Text = sum_items.ToString() +" / " + List_acc.Count.ToString();
+                    }
+                    else
+                    {
+                        label3.Text = "ללא תיאור";
+                        label5.Text = "";
+                    }
 
                 }
             }
 
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            current_OID = -1;
+            Last_OID = -1;
+            next_OID = -1;
+            counter_items = 0;
+            List_acc.Clear();
+            IMapDocument mxd = ZoomNextError.ArcMap.Application.Document as IMapDocument;
+            IMap map = mxd.get_Map(0);
+            string gdb = AO.getLayerPathFromMxd(map, "חלקות לעריכה");
+
+
+
+            if (String.IsNullOrEmpty(gdb))
+            {
+                MessageBox.Show("שכבת חלקות לעריכה אינה קיימת במפה");
+            }
+            if (String.IsNullOrEmpty(gdb))
+            {
+                MessageBox.Show("שכבת חלקות לעריכה אינה קיימת במפה");
+            }
+            else
+            {
+                ITable table = AO.open_table(gdb, "Errors");
+                int sum_items = table.RowCount(null);
+                if (table != null)
+                {
+                    counter_items++;
+                    int counter = 0;
+                    ICursor cursor = AO.create_table_cursor_with_query(table, "\"OBJECTID\" > " + current_OID.ToString());
+                    IRow row = cursor.NextRow();
+                    while (counter < 1 && row != null)
+                    {
+                        if (counter_items < sum_items)
+                        {
+                            counter++;
+                            current_OID = row.OID;
+                            //MessageBox.Show(current_OID.ToString());
+                            string GushNum = row.Value[row.Fields.FindField("GushNum")];
+                            string ParcelNum = row.Value[row.Fields.FindField("ParcelNum")];
+                            string GushSfx = row.Value[row.Fields.FindField("GushSfx")];
+                            object ErrDescription = row.Value[row.Fields.FindField("ErrDescription")];
+
+                            string query = "OBJECTID < 0";
+                            if (GushNum == "0" && ParcelNum == "0" && ErrDescription != null)
+                            {
+                                if (ErrDescription.ToString().Contains("OID:"))
+                                {
+                                    //MessageBox.Show(System.Text.RegularExpressions.Regex.Match(ErrDescription.ToString(), @"\d+").Value);
+                                    string oid = System.Text.RegularExpressions.Regex.Match(ErrDescription.ToString(), @"\d+").Value;
+                                    query = "OBJECTID = " + oid;
+                                }
+                            }
+                            else
+                            {
+                                query = "GUSH_NUM = " + GushNum +
+                                    " AND PARCEL = " + ParcelNum +
+                                    " AND GUSH_SUFFIX = " + GushSfx;
+                            }
+
+                            IFeatureLayer flayer = AO.getLayerFromMxd(map, "חלקות לעריכה");
+
+                            AO.ZoomToSelection(mxd, flayer, query);
+                            List_acc.Add(row.OID);
+                            //MessageBox.Show(get(List_acc));
+                            row = cursor.NextRow();
+
+                            if (ErrDescription != null)
+                            {
+                                label3.Text = ErrDescription.ToString();
+                                label5.Text = sum_items.ToString() +" / " + List_acc.Count.ToString();
+                            }
+                            else
+                            {
+                                label3.Text = "ללא תיאור";
+                                label5.Text = "";
+                            }
+
+                        }
+                        else
+                        {
+                            current_OID = -1;
+                            counter_items = 0;
+                        }
+                    }
+                }
+            }
         }
     }
 }
